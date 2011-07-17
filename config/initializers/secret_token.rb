d359bf5847cad6bcdbe6360efb188b184a5c98fa
Edward Ocampo-Gooding <edward@edwardog.net>
2011-07-17 13:18:44 -0500

    Rails reboot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noladex::Application.config.secret_token = '3336c7685a64ca4ec3c8e1a2928142fd0ac4d7a3dd9e024246ad8dcb189c69729165b19e470b8af97b94fa10af3d4bbaf1ae8500388de5addd35402f344a3395'
