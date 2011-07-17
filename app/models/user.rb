357f5d0fd72a1b7fd3b1d5db5190a24eb5259027
Tritico <matthew@tritico.de>
2011-07-17 17:28:16 -0500

    working on add new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   MINIMUM_MISSIONS = 1
 	has_many :missions
+	accepts_nested_attributes_for :missions
 	
-	validates_presence_of :name, :email, :url_photo
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	
+	validates_presence_of :name, :email
 	validates :missions, :length => { :minimum => MINIMUM_MISSIONS }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110717192023) do
+ActiveRecord::Schema.define(:version => 20110717211400) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -48,6 +48,10 @@ ActiveRecord::Schema.define(:version => 20110717192023) do
     t.string   "url1"
     t.string   "url2"
     t.string   "url3"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
