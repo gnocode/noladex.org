4b84e8dbfec4b006e699dbe6ae020daa11158b92
Chris Coyier <chriscoyier@gmail.com>
2011-07-17 19:06:07 -0400

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,32 @@
 
 ActiveRecord::Schema.define(:version => 20110717205012) do
 
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "missions", :force => true do |t|
+    t.string   "statement"
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "neighborhoods", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
@@ -19,6 +45,9 @@ ActiveRecord::Schema.define(:version => 20110717205012) do
     t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url1"
+    t.string   "url2"
+    t.string   "url3"
     t.string   "openid_url"
   end
 
