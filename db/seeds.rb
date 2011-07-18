9e4f8279d81b90cea3d8606acab1f8e12374d40a
Tritico <matthew@tritico.de>
2011-07-17 20:02:41 -0500

    working on final add user details

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,17 @@
 # categories: create build, organize, heal, teach
 
 categories = []
-["Create", "Build", "Organize", "Heal", "Teach"].each do | name |
+  ["Create", "Build", "Organize", "Heal", "Teach"].each do | name |
 	categories << Category.create(:name => name)
 end
 
 # missions
 # 12 users
 
-dog_names = %W(Rover Fido WunderMut Scooby Lassie Spot Lucky Clifford Cujo Milo Blue K-9)
+#dog_names = %W(Rover Fido WunderMut Scooby Lassie Spot Lucky Clifford Cujo Milo Blue K-9)
 
-dog_names.each do |name|
-  u = User.new(:name => name, :email => "#{name}@dog.com", :url_photo => 'http://placedog.com/300/300')
-  u.missions.build(:statement => "To be awesome", :category => categories.sample)
-  u.save!
-end
+#dog_names.each do |name|
+#  u = User.new(:name => name, :email => "#{name}@dog.com", :url_photo => 'http://placedog.com/300/300')
+#  u.missions.build(:statement => "To be awesome", :category => categories.sample)
+#  u.save!
+#end
