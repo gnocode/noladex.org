09c3d833335becb68acb7b686d9ede86c8751402
Matthew Ling <matthew@incrediblethingsdepartment.com>
2011-07-19 20:58:35 +0200

    dissallow public destroying

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_filter :require_no_user, :only => [:new, :create]
-  before_filter :require_user, :only => [:show, :edit, :update]
+  before_filter :require_user, :only => [:show, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.xml
