716a2e2d96020e77c99dda670cf55f77c12725fc
Joe Ellis <joe@squarefour.net>
2011-07-20 20:17:22 -0500

    final fixes on mailer

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,6 @@
 class UserSessionsController < ApplicationController
 
-  before_filter :require_no_user, :only => [:new, :create]
+  # before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
 
   def new
@@ -17,10 +17,10 @@ class UserSessionsController < ApplicationController
   end
 
   def destroy  
-    @user_session = UserSession.find  
-    @user_session.destroy  
-    flash[:notice] = "Successfully logged out."  
-    redirect_to root_url  
+    @user_session = UserSession.find
+    @user_session.destroy
+    flash[:notice] = "Successfully logged out."
+    redirect_to root_url
   end
 
 end
