9965f1102da273742a7b342edb59868870863886
Joe Ellis <joe@squarefour.net>
2011-07-26 20:16:07 -0500

    setting config for noladexstaging

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,6 @@ class User < ActiveRecord::Base
     }
   }
 
-
   validates_presence_of :name, :email, :avatar_file_name
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
   
