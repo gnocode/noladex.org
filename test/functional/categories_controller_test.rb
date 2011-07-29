e0df72464a90315e26e760202a8c3f4248bc97de
Joe Ellis <joe@squarefour.net>
2011-07-28 22:18:52 -0500

    commented out unneeded categories controller + test

--- test/functional/categories_controller_test.rb
+++ test/functional/categories_controller_test.rb
@@ -1,49 +1,49 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionController::TestCase
-  setup do
-    @category = categories(:create)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:categories)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create category" do
-    assert_difference('Category.count') do
-      post :create, :category => {:name => 'New Category'}
-    end
-
-    assert_redirected_to category_path(assigns(:category))
-  end
-
-  test "should show category" do
-    get :show, :id => @category.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @category.to_param
-    assert_response :success
-  end
-
-  test "should update category" do
-    put :update, :id => @category.to_param, :category => @category.attributes
-    assert_redirected_to category_path(assigns(:category))
-  end
-
-  test "should destroy category" do
-    assert_difference('Category.count', -1) do
-      delete :destroy, :id => @category.to_param
-    end
-
-    assert_redirected_to categories_path
-  end
+  # setup do
+  #     @category = categories(:create)
+  #   end
+  # 
+  #   test "should get index" do
+  #     get :index
+  #     assert_response :success
+  #     assert_not_nil assigns(:categories)
+  #   end
+  # 
+  #   test "should get new" do
+  #     get :new
+  #     assert_response :success
+  #   end
+  # 
+  #   test "should create category" do
+  #     assert_difference('Category.count') do
+  #       post :create, :category => {:name => 'New Category'}
+  #     end
+  # 
+  #     assert_redirected_to category_path(assigns(:category))
+  #   end
+  # 
+  #   test "should show category" do
+  #     get :show, :id => @category.to_param
+  #     assert_response :success
+  #   end
+  # 
+  #   test "should get edit" do
+  #     get :edit, :id => @category.to_param
+  #     assert_response :success
+  #   end
+  # 
+  #   test "should update category" do
+  #     put :update, :id => @category.to_param, :category => @category.attributes
+  #     assert_redirected_to category_path(assigns(:category))
+  #   end
+  # 
+  #   test "should destroy category" do
+  #     assert_difference('Category.count', -1) do
+  #       delete :destroy, :id => @category.to_param
+  #     end
+  # 
+  #     assert_redirected_to categories_path
+  #   end
 end
